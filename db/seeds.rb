3d8f51d0844c3fdf330625ea685fd82540e28038
Rodrigo Lima <rodrigo.cerqueira@gmail.com>
2021-10-25 11:11:42 -0300

    Inclusao heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+puts 'Colhendo dados da API. Iniciando...'
+url = 'https://tmdb.lewagon.com/movie/top_rated'
+json = URI.open(url).read
+hash = JSON.parse(json)
+puts 'Término. Dados coletados.'
+
+puts 'Início de limpeza do DB'
+Movie.destroy_all
+puts 'Fim da limpeza do DB'
+
+puts 'início da importação dos filmes ao banco de dados'
+hash['results'].each do |result|
+  movie = Movie.create!(
+    title: result['title'],
+    overview: result['overview'],
+    poster_url: "https://image.tmdb.org/t/p/original/#{result['poster_path']}",
+    rating: result['vote_average']
+  )
+
+  puts "Foi importado o filme #{movie.id}."
+end
